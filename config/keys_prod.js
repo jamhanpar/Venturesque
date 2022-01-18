011e8cfd7aba573669ac8740043cfce74ccf81d8
James <james.hansung.park@gmail.com>
2022-01-17 20:53:53 -0500

    added product keys

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  //   secretOrKey: "w9uF5ukaUwkZnU0M7jOlmycCLXvAd8Tb",
+  YELP_API_KEY: process.env.YELP_API_KEY,
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  ACCU_API_KEY: process.env.ACCU_API_KEY,
+};
